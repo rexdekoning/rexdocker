a3112238afdefed7cb00ab510f6a813d1236a9c1
rexdekoning <rexdekoning@gmail.com>
2017-05-29 17:17:01 +0200

    Add files via upload

---
+++ Start-nginx.ps1
@@ -0,0 +1,80 @@
+<#
+as admin:
+Register-PSRepository -Name DockerPS-Dev -SourceLocation https://ci.appveyor.com/nuget/docker-powershell-dev
+Install-Module Docker -Repository DockerPS-Dev
+#>
+Import-Module Docker
+Clear-Host
+
+function Start-Containers
+{
+    Write-Verbose -Message 'Make sure local folder containerlogs exists' -Verbose
+    if ((Test-Path -Path c:\containerlogs) -eq $false) {
+        New-Item c:\ContainerLogs -ItemType Directory
+        Write-Verbose -Message 'Folder containerlogs created' -Verbose
+    } else {
+        Write-Verbose -Message 'Folder containerlogs already exists' -Verbose
+    }
+
+    Write-Verbose -Message 'Read template file NGINX config files' -Verbose
+    $ConfigString = Get-Content -Path C:\ContainerLogs\nginx\template.conf -Raw
+
+    Write-Verbose -Message 'Starting Nodejs1 image' -Verbose
+    Invoke-ContainerImage -ImageIdOrName webserver -Name nodejs1 -Detach -Terminal -Input
+    $ipaddress = docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" nodejs1
+    $server = $ConfigString.Replace("[servername]", "nodejs1.foorbar.net")
+    Write-Verbose -Message "IPAddress node1" -Verbose
+    $ipaddress
+    $server = $server.Replace("[ipaddress]", $ipaddress)
+    $server | Out-File -FilePath C:\ContainerLogs\nginx\conf\nodejs1.conf -Encoding ascii
+    
+    Write-Verbose -Message 'Starting Nodejs2 image' -Verbose
+    Invoke-ContainerImage -ImageIdOrName webserver -Name nodejs2 -Detach -Terminal -Input
+    $ipaddress = docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" nodejs2
+    $server = $ConfigString.Replace("[servername]", "nodejs2.foorbar.net")
+    Write-Verbose -Message "IPAddress node2" -Verbose
+    $ipaddress
+    $server = $server.Replace("[ipaddress]", $ipaddress)
+    $server | Out-File -FilePath C:\ContainerLogs\nginx\conf\nodejs2.conf -Encoding ascii
+
+    Write-Verbose -Message 'Setting default config for NGINX image' -Verbose
+    $config = [Docker.DotNet.Models.Config]::new()
+    ($config.ExposedPorts  = [System.Collections.Generic.Dictionary[string,object]]::new()).Add("80/tcp", $null)
+    $hostConfig = [Docker.DotNet.Models.HostConfig]::new()
+    ($hostConfig.Binds = [System.Collections.Generic.List[string]]::New()).Add('C:\ContainerLogs\:c:\local\')
+    $pb = new-object Docker.DotNet.Models.PortBinding
+    $pb.HostPort = "80"
+    $hostConfig.PortBindings = [System.Collections.Generic.Dictionary[string, System.Collections.Generic.iList[Docker.DotNet.Models.PortBinding]]]::new()
+    $hostConfig.PortBindings.Add("80/tcp",[System.Collections.Generic.List[Docker.DotNet.Models.PortBinding]]::new([Docker.DotNet.Models.PortBinding[]]@($pb)))
+
+    Write-Verbose -Message 'Starting NGINX image' -Verbose
+    Invoke-ContainerImage -ImageIdOrName nginx-nanoserver -Configuration $config -Name nginx -Detach -Terminal -Input -HostConfiguration $hostConfig
+    $ipaddress = docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" nginx
+    Write-Verbose -Message "IPAddress nginx" -Verbose
+    $ipaddress
+}
+
+function Enter-AdminSession([string]$ContainerName)
+{
+    Enter-PSSession -ContainerId (Get-Container -Name $ContainerName).ID -RunAsAdministrator
+}
+
+function Stop-Containers 
+{
+    Write-Verbose -Message 'Stop containers' -Verbose
+    Get-Container | Stop-Container
+    Write-Verbose -Message 'Remove container' -Verbose
+    Get-Container | Remove-Container
+    Write-Verbose -Message 'Remove generated nginx config files' -Verbose
+    Remove-Item -Path C:\ContainerLogs\nginx\conf\nodejs1.conf
+    Remove-Item -Path C:\ContainerLogs\nginx\conf\nodejs2.conf
+}
+
+Start-Containers
+<#
+Enter-AdminSession 'nodejs1'
+Enter-AdminSession 'nodejs2'
+Enter-AdminSession 'nginx'
+Get-Container # List Running Containers
+Stop-Containers
+#>
---
+++ docker.ps1
@@ -0,0 +1,51 @@
+﻿<#
+as admin:
+Register-PSRepository -Name DockerPS-Dev -SourceLocation https://ci.appveyor.com/nuget/docker-powershell-dev
+Install-Module Docker -Repository DockerPS-Dev
+#>
+Import-Module Docker
+Clear-Host
+
+if ((Test-Path -Path c:\containerlogs) -eq $false) {
+    New-Item c:\ContainerLogs -ItemType Directory
+    Write-Verbose -Message 'Folder containerlogs created' -Verbose
+} else {
+    Write-Verbose -Message 'Folder containerlogs already exists' -Verbose
+}
+
+Write-Verbose -Message 'Starting container nano' -Verbose
+
+$hostConfig = [Docker.DotNet.Models.HostConfig]::new()
+($hostConfig.Binds = [System.Collections.Generic.List[string]]::New()).Add('C:\ContainerLogs\:c:\logs\')
+$pb = new-object Docker.DotNet.Models.PortBinding
+$pb.HostPort = "81"
+$hostConfig.PortBindings = [System.Collections.Generic.Dictionary[string, System.Collections.Generic.iList[Docker.DotNet.Models.PortBinding]]]::new()
+$hostConfig.PortBindings.Add("80/tcp",[System.Collections.Generic.List[Docker.DotNet.Models.PortBinding]]::new([Docker.DotNet.Models.PortBinding[]]@($pb)))
+
+Invoke-ContainerImage -ImageIdOrName nanoserver/iis -Name nano -Detach -Terminal -Input -HostConfiguration $hostConfig
+#Enter-PSSession -ContainerId (Get-Container -Name nano).ID -RunAsAdministrator
+
+Write-Verbose -Message 'Show IP' -Verbose
+$ipaddress = docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" nano
+$ipaddress
+Write-Verbose -Message 'Stopping default website' -Verbose
+Invoke-Command -ContainerId (Get-Container -Name nano).ID -ScriptBlock {import-module IISAdministration; Stop-IISSite $args[0] -Confirm:$false;} -ArgumentList @('Default Web Site') -RunAsAdministrator
+Write-Verbose -Message 'Create new website' -Verbose
+Invoke-Command -ContainerId (Get-Container -Name nano).ID -ScriptBlock {import-module IISAdministration; New-IISSite -Name TestSite -BindingInformation "*:80:" -PhysicalPath c:\logs} -RunAsAdministrator
+Write-Verbose -Message 'Start default website' -Verbose
+Invoke-Command -ContainerId (Get-Container -Name nano).ID -ScriptBlock {import-module IISAdministration; Start-IISSite $args[0];} -ArgumentList @('TestSite') -RunAsAdministrator
+$site = Invoke-WebRequest -Uri http://$ipaddress/default.aspx
+Write-Verbose -Message 'Write Website Content' -Verbose
+Write-Verbose -Message 'Get computername via psremote' -Verbose
+Invoke-Command -ContainerId (Get-Container -Name nano).ID -ScriptBlock { $env:computername }
+
+Write-Verbose -Message 'Kill container' -Verbose
+Stop-Container -ContainerIdOrName nano
+Write-Verbose -Message 'Container stopped but still exists' -Verbose
+Get-Container -ContainerIdOrName nano
+Write-Verbose -Message 'Remove container' -Verbose
+#docker rm nano
+Remove-Container -ContainerIdOrName nano
+Write-Verbose -Message 'Now the container is really gone and we can re-use the name' -Verbose
+Get-Container | fl *
+
